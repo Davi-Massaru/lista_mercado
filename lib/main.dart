3e4698d7a8283e4e2c9b12e01d59e7786769f33f
massaru <Davi-Massaru>
2021-04-06 16:45:08 -0300

    upload the project

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:lista_mercado/routes.dart';
+
+void main() => runApp(Routes());
+
---
+++ lib/models/Item.dart
@@ -0,0 +1,11 @@
+class Item{
+  String id;
+  String name;
+  String note;
+  int quantity;
+  bool caught;
+  bool without;
+
+  Item({this.id,  this.name,  this.note,  this.quantity = 1,    this.caught = false,  this.without = false});
+
+}
